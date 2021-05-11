c2a54a6eff252623f705032e3edbf7531ba4f7d9
Guido-95 <guidocarna1995@gmail.com>
2021-05-11 14:52:57 +0200

    esercizio 1

---
+++ snack 1/js/script.js
@@ -0,0 +1,19 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+// array di oggetti bici
+var bici = [
+    {
+        nome:"bici da montagna",
+        peso: 10
+    }
+    ,
+    {
+        nome:"bici da strada",
+        peso: 12
+    }
+]
+
+// stampa bici con peso minore
+console.log("bici con peso minore",bici[0]);
